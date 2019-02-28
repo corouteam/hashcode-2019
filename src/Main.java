2aead28cdc939439acfbc3810c426bda7080e7d7
Paolo Rotolo <paolorotolo@ubuntu.com>
2019-02-28 19:15:14 +0100

    Happy Commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] Args) {
+    public static void main(String[] Args) throws IOException {
         new Main().init();
     }
 
-    void init(){
+    void init() throws IOException {
         System.out.println("Hashcode 2019!");
+
+        ArrayList<Photo> photos;
+        List<String> input = HappyParser.FileToStringArray("a_example.txt");
+                for (int i = 0; i < input.size(); i++){
+                    String[] splitInput = input.get(i).split(" ");
+                    for (int i = 2; )
+                    photos[i] = new Photo(splitInput.)
+
+        }
     }
 }
---
+++ src/Photo.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Photo {
+    int id;
+    String orientation;
+    HashSet<String> tags;
+
+    public Photo(int id, String orientation, HashSet<String> tags) {
+        this.id = id;
+        this.orientation = orientation;
+        this.tags = tags;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+}
